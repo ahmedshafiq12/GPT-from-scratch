901e0ca3c13751cba56d10e8259775e32298ad41
Ahmed Shafiq <ahmedmuhammedshafiq@gmail.com>
2024-06-07 16:03:29 +0300

    DataLoader is added

--- GPT/__init__.py
+++ GPT/__init__.py
@@ -2,4 +2,4 @@ from .Head import Head
 from .MultiHeadAttention import MultiHeadAttention
 from .FeedForward import FeedForward
 from .Block import Block
-from .GPTLanguageModel import GPTLanguageModel
+from .LanguageModel import GPTLanguageModel
